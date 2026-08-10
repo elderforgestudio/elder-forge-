a9b05e6edf96482447070d6466603a59f416d28b
elderforgestudio <elderforge0@gmail.com>
2026-08-10 12:45:08 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 const menuAll = document.querySelector('.all')
+const detalist = document.querySelector('.detalistt')
 
 const menuLateral = document.querySelector('.menuLateral')
 
 menuAll.addEventListener('click',(c) => {
     let target = c.target
-    console.log(c)
+    console.log(target.className)
     if (target.className == 'material-symbols-outlined') {
         menuLateral.classList.toggle('menuLateralA')
     }
+    if (target.className == 'dowland') {
+        detalist.setAttribute('open', '') 
+    }
+
 })
